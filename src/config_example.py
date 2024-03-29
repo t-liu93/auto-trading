12283f971e2c2a913b483ca99162cf3580344296
Tianyu Liu <info@jamesvillage.dev>
2024-03-29 12:52:01 +0100

    Add example config

---
+++ src/config_example.py
@@ -0,0 +1,10 @@
+USERNAME = '<your tradingview username>'
+PASSWORD = '<your tradingview password>'
+
+# list of symbols, if exchange unkown or with spaces, leave it empty
+LIST_OF_SYMBOLS = [
+    {'symbol': 'BTCUSD', 'exchange': 'COINBASE', 'interval': '1'}
+]
+
+WEBHOOK = '<your discord webhook>'
+
