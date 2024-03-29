e2493d5c31cf8c90a012e158c623a5b0bbf3661e
Tianyu Liu <info@jamesvillage.dev>
2024-03-29 14:43:16 +0100

    Use .env instead of plain text password

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+WEBHOOK = os.getenv("WEBHOOK")
+
+LIST_OF_SYMBOLS = [{"symbol": "BTCUSD", "exchange": "COINBASE", "interval": "1"}]
