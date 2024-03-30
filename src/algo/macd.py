966f9f4b0d689c8a2c54e6be2f6a257c4370138b
Tianyu Liu <info@jamesvillage.dev>
2024-03-30 14:29:49 +0100

    MACD using sma

--- src/algo/macd.py
+++ src/algo/macd.py
@@ -2,7 +2,7 @@ import pandas as pd
 from algo.algo import Algo, Trend
 from talib import abstract
 
-MACD = abstract.MACD
+MACD = abstract.MACDEXT
 
 MACD_FAST = 7
 MACD_SLOW = 12
@@ -11,6 +11,9 @@ MACD_SIGNAL = 9
 MACD_THRESHOLD = 0
 MACD_TREND_BACK_LENGTH = 1
 
+MA_TYPE_SMA = 0
+MA_TYPE_EMA = 1
+
 
 class Macd(Algo):
     def __init__(self) -> None:
@@ -18,7 +21,16 @@ class Macd(Algo):
         self._macds: pd.DataFrame = {}
 
     def prepare_macds(self, prices: pd.DataFrame) -> None:
-        self._macds = MACD(prices, fastperiod=MACD_FAST, slowperiod=MACD_SLOW, signalperiod=MACD_SIGNAL).iloc[-25:]
+        self._macds = MACD(
+            prices,
+            fastperiod=MACD_FAST,
+            fastmatype=MA_TYPE_SMA,
+            slowperiod=MACD_SLOW,
+            slowmatype=MA_TYPE_SMA,
+            signalperiod=MACD_SIGNAL,
+            signalmatype=MA_TYPE_EMA,
+        ).iloc[-25:]
+        print(self._macds)
 
     def _calculate_macd(self, prices: pd.DataFrame) -> None:
         new_macds = MACD(prices, fastperiod=MACD_FAST, slowperiod=MACD_SLOW, signalperiod=MACD_SIGNAL).iloc[-1:]
