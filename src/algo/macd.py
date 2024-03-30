0b0c16c999db2d5626863b0c084173601abdc69d
Tianyu Liu <info@jamesvillage.dev>
2024-03-30 20:08:09 +0100

    MACD now determin using either sma or ema

--- src/algo/macd.py
+++ src/algo/macd.py
@@ -14,37 +14,74 @@ MACD_TREND_BACK_LENGTH = 1
 MA_TYPE_SMA = 0
 MA_TYPE_EMA = 1
 
+MA_TYPE = MA_TYPE_SMA
+
 
 class Macd(Algo):
     def __init__(self) -> None:
         super().__init__()
-        self._macds: pd.DataFrame = {}
+        self._macds: dict[str, pd.DataFrame] = {}
 
     def prepare_macds(self, prices: pd.DataFrame) -> None:
-        self._macds = MACD(
+        self._macds["sma"] = MACD(
             prices,
             fastperiod=MACD_FAST,
             fastmatype=MA_TYPE_SMA,
             slowperiod=MACD_SLOW,
             slowmatype=MA_TYPE_SMA,
             signalperiod=MACD_SIGNAL,
+            signalmatype=MA_TYPE_SMA,
+        ).iloc[-25:]
+        self._macds["ema"] = MACD(
+            prices,
+            fastperiod=MACD_FAST,
+            fastmatype=MA_TYPE_EMA,
+            slowperiod=MACD_SLOW,
+            slowmatype=MA_TYPE_EMA,
+            signalperiod=MACD_SIGNAL,
             signalmatype=MA_TYPE_EMA,
         ).iloc[-25:]
-        print(self._macds)
 
     def _calculate_macd(self, prices: pd.DataFrame) -> None:
-        new_macds = MACD(prices, fastperiod=MACD_FAST, slowperiod=MACD_SLOW, signalperiod=MACD_SIGNAL).iloc[-1:]
-        macd_t = self._macds.T
-        macd_t.pop(self._macds.iloc[0].name)
-        self._macds = macd_t.T
-        self._macds = pd.concat([self._macds, new_macds], axis=0)
+        new_macds_sma = MACD(
+            prices,
+            fastperiod=MACD_FAST,
+            fastmatype=MA_TYPE_SMA,
+            slowperiod=MACD_SLOW,
+            slowmatype=MA_TYPE_SMA,
+            signalperiod=MACD_SIGNAL,
+            signalmatype=MA_TYPE_SMA,
+        ).iloc[-1:]
+        new_macds_ema = MACD(
+            prices,
+            fastperiod=MACD_FAST,
+            fastmatype=MA_TYPE_EMA,
+            slowperiod=MACD_SLOW,
+            slowmatype=MA_TYPE_EMA,
+            signalperiod=MACD_SIGNAL,
+            signalmatype=MA_TYPE_EMA,
+        ).iloc[-1:]
+        macd_sma_t = self._macds["sma"].T
+        macd_sma_t.pop(self._macds["sma"].iloc[0].name)
+        macd_ema_t = self._macds["ema"].T
+        macd_ema_t.pop(self._macds["ema"].iloc[0].name)
+        self._macds["sma"] = macd_sma_t.T
+        self._macds["ema"] = macd_ema_t.T
+        self._macds["sma"] = pd.concat([self._macds["sma"], new_macds_sma], axis=0)
+        self._macds["ema"] = pd.concat([self._macds["ema"], new_macds_ema], axis=0)
 
     def make_decision(self, prices: pd.DataFrame) -> int:
         self._calculate_macd(prices)
-        latest_macd = self._macds.iloc[-1]["macd"]
-        old_macd = self._macds.iloc[-1 - MACD_TREND_BACK_LENGTH]["macd"]
-        if latest_macd > MACD_THRESHOLD and Algo.determine_trend(old_macd, latest_macd) == Trend.FALLING:
+        latest_macd_sma = self._macds["sma"].iloc[-1]["macd"]
+        latest_macd_ema = self._macds["ema"].iloc[-1]["macd"]
+        old_macd_sma = self._macds["sma"].iloc[-1 - MACD_TREND_BACK_LENGTH]["macd"]
+        old_macd_ema = self._macds["ema"].iloc[-1 - MACD_TREND_BACK_LENGTH]["macd"]
+        if (latest_macd_sma > MACD_THRESHOLD and Algo.determine_trend(old_macd_sma, latest_macd_sma) == Trend.FALLING) or (
+            latest_macd_ema > MACD_THRESHOLD and Algo.determine_trend(old_macd_ema, latest_macd_ema == Trend.FALLING)
+        ):
             return -1
-        if latest_macd < MACD_THRESHOLD and Algo.determine_trend(old_macd, latest_macd) == Trend.RISING:
+        if (latest_macd_sma < MACD_THRESHOLD and Algo.determine_trend(old_macd_sma, latest_macd_sma) == Trend.RISING) or (
+            latest_macd_ema < MACD_THRESHOLD and Algo.determine_trend(old_macd_ema, latest_macd_ema == Trend.RISING)
+        ):
             return 1
         return 0
